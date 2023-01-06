6f090ab41116845168f3fc38943ee150a7560e79
tommysoprano <thomasjaymaly@gmail.com>
2023-01-06 17:17:22 -0500

    coppied psuedo

--- Pseudo_Code/db/seed.sql
+++ Pseudo_Code/db/seed.sql
@@ -1,8 +1,10 @@
 -- USE your_database;
-
+USE staff_db;
 
 -- INSERT INTO your_table_for_departments
 
 -- INSERT INTO your_table_for_roles
+INSERT INTO roles ()
 
 -- INSERT INTO your_table_for_employees
+INSERT INTO employee
