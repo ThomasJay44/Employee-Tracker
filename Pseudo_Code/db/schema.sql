4e52bffecc0aff3a301634b5517546176546700d
tommysoprano <thomasjaymaly@gmail.com>
2023-01-06 19:36:53 -0500

    added query page

--- Pseudo_Code/db/schema.sql
+++ Pseudo_Code/db/schema.sql
@@ -1,31 +1,28 @@
-DROP DATABASE IF EXISTS staff_db;
-CREATE DATABASE staff_db;
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
 
-USE staff_db;
+USE employee_db;
 
 CREATE TABLE department (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      name VARCHAR(30)
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(30) NOT NULL
 );
 
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary INT NOT NULL,
     department_id INT NOT NULL,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE,
-    INDEX dep_ind (department_id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL,
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
-    manager_id INT UNSIGNED,
-    INDEX man_ind (manager_id)
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
